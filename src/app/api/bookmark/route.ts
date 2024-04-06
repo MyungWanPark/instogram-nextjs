e9655137233b83e674da8610cea5cf03eef088f8
MyungWan Park <mywanpark@gmail.com>
2024-04-06 15:16:51 +0900

    feat: service layer of save and remove bookmark

---
+++ src/app/api/bookmark/route.ts
@@ -0,0 +1,24 @@
+import { authOptions } from "@/auth/authOptions";
+import { addBookmark, removeBookmark } from "@/service/user";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PUT(req: NextRequest) {
+    const session = await getServerSession(authOptions);
+    const user = session?.user;
+
+    if (!user) {
+        return new Response("Authentication Error", { status: 401 });
+    }
+
+    const { id, bookmark } = await req.json();
+
+    if (!id || bookmark === undefined) {
+        return new Response("Bad request", { status: 400 });
+    }
+
+    const request = bookmark ? addBookmark : removeBookmark;
+    return request(user.id, id)
+        .then((res) => NextResponse.json(res))
+        .catch((err) => new Response(JSON.stringify(err), { status: 500 }));
+}
--- src/service/user.ts
+++ src/service/user.ts
@@ -70,3 +70,18 @@ export async function getUserProfileBy(username: string): Promise<ProfileUser> {
             posts: user.posts ?? 0,
         }));
 }
+
+export async function addBookmark(userId: string, postId: string) {
+    return client
+        .patch(userId)
+        .setIfMissing({ bookmark: [] })
+        .append("bookmark", [{ _ref: postId, _type: "reference" }])
+        .commit({ autoGenerateArrayKeys: true });
+}
+
+export async function removeBookmark(userId: string, postId: string) {
+    return client
+        .patch(userId)
+        .unset([`bookmark[_ref == "${postId}"]`])
+        .commit();
+}
