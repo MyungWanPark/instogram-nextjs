029c458b1f50fe8e22e37f568bbf4a2c79904084
MyungWan Park <mywanpark@gmail.com>
2024-04-14 21:02:13 +0900

    feat: middleware for pages and api router handler

---
+++ src/middleware.ts
@@ -0,0 +1,38 @@
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+export { default } from "next-auth/middleware";
+
+export async function middleware(req: NextRequest) {
+    const token = await getToken({ req });
+    const { pathname, search, origin, basePath } = req.nextUrl;
+
+    if (!token) {
+        if (req.nextUrl.pathname.startsWith("/api")) {
+            return new NextResponse("Authentication Error", { status: 401 });
+        }
+
+        const { pathname, search, origin, basePath } = req.nextUrl;
+
+        const signInUrl = new URL(`${basePath}/auth/signin`, origin);
+        signInUrl.searchParams.append(
+            "callbackUrl",
+            `${basePath}${pathname}${search}`
+        );
+        return NextResponse.redirect(signInUrl);
+    }
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: [
+        "/new",
+        "/",
+        "/api/bookmarks",
+        "/api/comments",
+        "/api/likes",
+        "/api/follow",
+        "/api/me",
+        "/api/posts/:path*",
+    ],
+};
